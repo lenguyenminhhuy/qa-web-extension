06082bd1633a17498290d68f96a779f9326e4e49
s3697272 <s3697272@users.noreply.github.com>
2021-03-28 05:42:13 +0700

    remove

--- extension/assets/js/getinput.js
+++
@@ -1,7 +0,0 @@
-// var inputs = document.getElementsByTagName('input');
-
-// for(var i = 0; i < inputs.length; i++) {
-//     if(inputs[i].type.toLowerCase() == 'text') {
-//         alert(inputs[i].value);
-//     }
-// }
