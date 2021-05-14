106e5b1756fa9b3a1093a87c74a98b46991528bd
witm15 <s3697272@rmit.edu.vn>
2021-05-14 19:44:25 +0700

    [UPDATE] update format code

--- server_extension/index.js
+++ server_extension/index.js
@@ -1,23 +1,21 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-var cors = require('cors')
-const app = express()
-const db = require('./queries')
-const port = 3000
+const express = require("express");
+const bodyParser = require("body-parser");
+var cors = require("cors");
+const app = express();
+const db = require("./queries");
+const port = 3000;
 
-
-app.use(cors())
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use(require('body-parser').json()); 
-app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require("body-parser").json());
+app.use(require("body-parser").urlencoded({ extended: true }));
 
-app.get('/', (request, response) => {
-  response.json({ info: 'extension' })
-})
+app.get("/", (request, response) => {
+    response.json({ info: "extension" });
+});
 
-
-app.post('/rating_answer', db.addBestAnswer)
+app.post("/rating_answer", db.addBestAnswer);
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}.`)
-})
+    console.log(`App running on port ${port}.`);
+});
